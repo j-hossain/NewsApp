5cef8db6a82a32bcf6eccf4207943089e6261f24
d-joker <jahin.hsn101@gmail.com>
2023-04-03 19:40:44 +0600

    Retrofit added

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "09cb16e12a694e25a4c7379d9cd6bc7a"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
