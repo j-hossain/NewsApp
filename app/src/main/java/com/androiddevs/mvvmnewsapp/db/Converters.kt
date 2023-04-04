d58e7b617cae99f0d7ba84a0bbf4f46f0a50cd07
d-joker <jahin.hsn101@gmail.com>
2023-04-04 20:11:19 +0600

    article adapter created

--- app/src/main/java/com/androiddevs/mvvmnewsapp/db/Converters.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/Converters.kt
@@ -8,6 +8,7 @@ class Converters {
     fun fromSource(source: Source):String{
         return source.name
     }
+    @TypeConverter
     fun toSource(name: String): Source {
         return  Source(name,name)
     }
