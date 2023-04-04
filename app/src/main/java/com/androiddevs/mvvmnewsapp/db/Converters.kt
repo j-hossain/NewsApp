79248f444fd31db5550f0f2ea92d145346710eac
d-joker <jahin.hsn101@gmail.com>
2023-04-04 14:14:17 +0600

    Article Database setup done

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/Converters.kt
@@ -0,0 +1,14 @@
+package com.androiddevs.mvvmnewsapp.db
+
+import androidx.room.TypeConverter
+import com.androiddevs.mvvmnewsapp.models.Source
+
+class Converters {
+    @TypeConverter
+    fun fromSource(source: Source):String{
+        return source.name
+    }
+    fun toSource(name: String): Source {
+        return  Source(name,name)
+    }
+}
