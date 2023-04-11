e552692f8cdbd18596e49d10a1e31be9b1055055
d-joker <jahin.hsn101@gmail.com>
2023-04-11 15:21:19 +0600

    pagination complete for breaking news and search Page

--- app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsApi.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsApi.kt
@@ -10,7 +10,7 @@ interface NewsApi {
     @GET("v2/top-headlines")
     suspend fun getBreakingNews(
         @Query("country")
-        countryCode: String = "us",
+        countryCode: String = "in",
         @Query("page")
         pageNumber: Int = 1,
         @Query("apikey")
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "09cb16e12a694e25a4c7379d9cd6bc7a"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
